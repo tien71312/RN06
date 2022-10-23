8a61c7cfd5d22bd6a41a88ddffa842c4ad2d4a03
Dong Huu Tien <dh71312@gmail.com>
2022-10-23 20:05:03 +0700

    Update buổi 6

--- buoi_4/DEMO_BUOI4/RN01/screens/contact/DataBiding.js
+++ buoi_4/DEMO_BUOI4/RN01/screens/contact/DataBiding.js
@@ -5,8 +5,15 @@ export default class DataBiding extends Component {
     //SQLite, Data...
     // Khi gọi setState() thì sẽ  rerender lại giao diện
     state={
-        ketQua:0
+        ketQua:0,
+        count:0
     }
+    data = [
+        'Hello',
+        'Cyber',
+        'React native'
+    ]
+
     hello='Hello data biding'
     soA=6
    // ketQua=0
@@ -19,29 +26,52 @@ export default class DataBiding extends Component {
         return kq 
     }
     _onClickTinhTong=(so1,so2)=>{
-        // Thay đổi giá trị của thuộc tính trong state
+        // // Thay đổi giá trị của thuộc tính trong state
+        // this.setState({
+        //     ketQua:this.tinhTong(so1,so2)
+        // }) 
+        // console.log(`Kết quả là ${this.state.ketQua}`)
+        let tangCount =this.state.count += 1
         this.setState({
-            ketQua:this.tinhTong(so1,so2)
-        }) 
-        console.log(`Kết quả là ${this.state.ketQua}`)
+            count:tangCount
+        })
     }
 
+    _renderDesc =()=>{
+        return (
+            <Text>{this.state.count==5 ? this.contact.desc : ''}</Text>
+        )
+    }
+    _renderArray =() =>{
+       return this.data.map((data,index)=>{
+            return <Text>{data}</Text>
+        } )
+    }
+    //so sánh 3 ngôi
+    //Cú pháp: điều kiện ? đúng : sai
   render() {
     let soB=10
+    console.log('Render ' + this.state.count)
     return (
       <View style={{
         flex:1,
         justifyContent:'center',
         alignItems:'center'
       }}>
-        <Text>{this.contact.title}</Text>
+        {this._renderDesc()}
+        {/* <Text>{this.state.count==5 ? this.contact.desc : ''}</Text> */}
         {/* <Text>{this.tinhTong(this.soA,soB)}</Text> */}
-        <Text>{this.state.ketQua}</Text>
+        <Text>{this.state.count}</Text>
         <TouchableOpacity
             onPress={()=>this._onClickTinhTong(this.soA,soB)}
         >
             <Text>Tính tổng</Text>
         </TouchableOpacity>
+        {/* {this.data.map((value, index)=>{
+             return   <Text>{value}</Text>
+        })} */}
+
+       { this._renderArray()}
       </View>
     )
   }
