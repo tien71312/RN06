f6ccb09c19d33ca529e9245b519a0cd713a52d1a
Dong Huu Tien <dh71312@gmail.com>
2022-10-25 17:46:54 +0700

    Cách dùng SectionList

--- buoi_4/DEMO_BUOI4/RN01/screens/newfeed/Newfeed.js
+++ buoi_4/DEMO_BUOI4/RN01/screens/newfeed/Newfeed.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ScrollView, FlatList } from 'react-native'
+import { View, Text, Image, ScrollView, FlatList, SectionList } from 'react-native'
 import React from 'react'
 import styles from './styles/styles'
 import AppBar from './component/AppBar'
@@ -8,12 +8,51 @@ import AppBar from './component/AppBar'
 //hook 
 
 export default function Newfeed() {
-let listData = [
-    1,2,3,4,5,6,7,8,9,10
+// let listData = [
+//     1,2,3,4,5,6,7,8,9,10
+// ]
+
+let dataListSections=[
+    {
+        title:'A',
+        data: [
+            'Nguyễn Văn A',
+            'Nguyễn Hữu Ánh',
+            'Lê thị An'
+        ]
+    },
+    {
+        title:'B',
+        data: [
+            'Nguyễn Văn B',
+            'Nguyễn Hữu Bình',
+            'Lê thị Bính'
+        ]
+    },
+    {
+        title:'C',
+        data: [
+            'Nguyễn Văn C',
+            'Nguyễn Hữu Chanh',
+            'Lê thị Chảnh'
+        ]
+    }
 ]
+
 _renderItem= (data)=>{
     return <Text style={{height:150}}>{data.item}</Text>
 }
+_renderHeaderItem=(data) => {
+   // console.log(data)
+    return <Text>{data.section.title}</Text>
+}
+_renderItemList= (data)=> {
+    //console.log(data)
+    return <Text>{data.item}</Text>
+}
+
+
+
   return (
     <View style={styles.container}>
     <AppBar title='New Feed' />
@@ -21,13 +60,19 @@ _renderItem= (data)=>{
         <Text>bla bla....</Text>
     </ScrollView>
    */}
-    <FlatList 
+    {/* <FlatList 
         data={listData} // bắt buộc phải là một giá trị mảng
         renderItem={(data)=>_renderItem(data)} // bắt buộc trả ra giao diện
-        keyExtractor={(data)=>data.toString()}
-       
-
+        keyExtractor={(data)=>data.toString()} 
+    /> */}
+    <SectionList 
+        sections={dataListSections} //nhận vào dữ liệu
+        renderSectionHeader={(data)=>_renderHeaderItem(data)} //trả ra giao diện cho Header
+        renderItem={(data)=>_renderItemList(data)} // trả ra giao diện cho thành phần con trong header
+    
     />
+
+
     </View>
   )
 }
