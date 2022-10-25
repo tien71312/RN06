0e1aea3cdd0812c52d226ad5f7bc2297069f9255
Dong Huu Tien <dh71312@gmail.com>
2022-10-25 15:01:05 +0700

    Bài tập Emoji

--- buoi_4/DEMO_BUOI4/RN01/index.js
+++ buoi_4/DEMO_BUOI4/RN01/index.js
@@ -7,6 +7,7 @@ import App from './App';
 import Login from './screens/login/Login';
 import DataBiding from './screens/contact/DataBiding';
 import Contact from './screens/contact/Contact';
+import Emoji from './screens/emoji/Emoji';
 import {name as appName} from './app.json';
  
-AppRegistry.registerComponent(appName, () => Contact);
+AppRegistry.registerComponent(appName, () => Emoji);
