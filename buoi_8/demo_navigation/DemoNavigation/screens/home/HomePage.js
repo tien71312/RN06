fc7b05cfcbbe44b91999f91eaaf6a61c116aeeff
Dong Huu Tien <dh71312@gmail.com>
2022-11-26 04:02:56 +0700

    Demo Navigation

---
+++ buoi_8/demo_navigation/DemoNavigation/screens/home/HomePage.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <View>
+        <Text>HomePage</Text>
+      </View>
+    )
+  }
+}
